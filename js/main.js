f343a1a9d45f6a36798edc8da90d009f83d45c6d
hayleypyle <hstowell@icloud.com>
2023-10-14 13:06:49 -0400

    Start of js file. Added two functions. giphySearch is just started.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-fetch('https://api.github.com/users/giphy/repos')
+const api_key ="cyOMBw01Y8fyiVDNEYTYZzRSfm0Va4Y2"
+
+let form = document.querySelector('.js-search-form')
+let searchInput = document.querySelector('[name=search-name]')
+
+function giphySearch(searchTerms){
+return fetch(`http://api.giphy.com/v1/gifs/search?q=${searchTerms}&api_key=${api_key}&limit=20`)
+.then(response => console.log(response)
+)}
+
+function submitSearch(event){
+    event.preventDefault();
+    let searchTerms = searchInput.value
+    giphySearch(searchTerms);
+
+}
+
+form.addEventListener('submit', submitSearch);
+
