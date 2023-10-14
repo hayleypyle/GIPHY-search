e68dc1a188a30f645553913f4face273844c1453
hayleypyle <hstowell@icloud.com>
2023-10-14 15:13:21 -0400

    added grid styling in css

--- js/main.js
+++ js/main.js
@@ -5,18 +5,18 @@ let searchInput = document.querySelector('[name=search-name]')
 let searchContainer = document.querySelector('.js-container')
 
 function giphySearch(searchTerms){
-fetch(`http://api.giphy.com/v1/gifs/search?q=${searchTerms}&api_key=${api_key}&limit=20`)
+fetch(`http://api.giphy.com/v1/gifs/search?q=${searchTerms}&api_key=${api_key}&limit=16`)
 .then (data => data.json())
 .then( response => {
-    let processedSearch = `<tr>` + response.data
+    let processedSearch = `<div class="grid-container">` + response.data
     .map(gif =>
 
-    `<td><iframe src="${gif.embed_url}" 
-    width="200" height="200" frameBorder="0" class="giphy-embed" 
+    `<div class="grid-item"><iframe src="${gif.embed_url}" 
+    width="250" height="250" frameBorder="0" class="giphy-embed" 
     allowFullScreen></iframe>
-    </td>`
+    </div>`
     )
-    .join('')+ '</tr>';
+    .join('')+ '</div>';
     searchContainer.innerHTML = processedSearch;
     })
     .catch(err => {
